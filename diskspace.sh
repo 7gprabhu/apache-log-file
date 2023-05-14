6e61f780846523024c95cde6117700666b6636cf
Prabhu Selvaraj <132800722+7gprabhu@users.noreply.github.com>
2023-05-15 00:26:01 +0800

    Create diskspace.sh

---
+++ diskspace.sh
@@ -0,0 +1,19 @@
+/path/to/logs/*.log {
+    daily
+    rotate 180
+    compress
+    delaycompress
+    notifempty
+    missingok
+    size 50M
+    create 640 root adm
+    sharedscripts
+    postrotate
+        /usr/bin/find /path/to/logs/ -type f -name '*.log.????-??-??' -mtime +210 -exec gzip {} \;
+    endscript
+    prerotate
+        if [ -d /path/to/logs/recent ]; then
+            /usr/bin/find /path/to/logs/ -type f -name '*.log' -mtime -90 -exec cp --preserve=timestamps {} /path/to/logs/recent \;
+        fi
+    endscript
+}
