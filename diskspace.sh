082c370f3d92e9f9ebc4d0d3a8fd63eb0d3c84f7
Prabhu Selvaraj <132800722+7gprabhu@users.noreply.github.com>
2023-05-15 00:27:10 +0800

    Update diskspace.sh

--- diskspace.sh
+++ diskspace.sh
@@ -9,7 +9,7 @@
     create 640 root adm
     sharedscripts
     postrotate
-        /usr/bin/find /path/to/logs/ -type f -name '*.log.????-??-??' -mtime +210 -exec gzip {} \;
+        /usr/bin/find /path/to/logs/ -type f -name '*.log.2023-05-15' -mtime +210 -exec gzip {} \;
     endscript
     prerotate
         if [ -d /path/to/logs/recent ]; then
